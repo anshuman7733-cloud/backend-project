8905906e6a71ee5fb1af59050c0a9c46d7534f55
Anshuman <anshuman7733@gmail.com>
2026-01-02 16:46:40 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
-  
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error: ", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`\nServer is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch();
 
 
 
