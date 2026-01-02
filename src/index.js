b68b98bc678cf021c5cbdfb2667b6b41ae8616d5
Anshuman <anshuman7733@gmail.com>
2026-01-02 14:26:50 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import dotenv  from "dotenv";
+import connectDB from "./db/index.js";
+
+
+
+connectDB()
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.log("Error: ", err);
+//       throw err;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on port ${process.env.PORT}`);
+//     });
+
+//   } catch (error) {
+//     console.error("Error: ", error);
+//     throw error;
+//   }
+// })();
